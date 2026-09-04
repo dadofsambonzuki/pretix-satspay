bd5a2e0feda9e6d87f9d06b539fadcd36df512a0
Nathan Day <nathan@day.ag>
2026-09-04 15:36:17 +0100

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_namespace_packages, setup
 
 setup(
     name="pretix-satspay",
-    version="0.1.0",
+    version="0.1.1",
     description="Accept Bitcoin payments in pretix via the LNbits Satspay extension",
     long_description="""# pretix Satspay
 
